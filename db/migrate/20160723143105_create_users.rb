38bdda93c7448d6b01df6677449ca04e43eacc0b
Xavier Serrano <zombispormedio007@gmail.com>
2016-07-23 18:07:24 +0200

    structure

---
+++ db/migrate/20160723143105_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :uid,                null: false, default: ""
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+    end
+  end
+end
