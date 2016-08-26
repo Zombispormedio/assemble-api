69e4e0b30b48d164343cc923b6b9505803faa76c
Xavier Serrano <zombispormedio007@gmail.com>
2016-08-26 17:11:44 +0200

    preview user

--- app/helpers/selection_helper.rb
+++ app/helpers/selection_helper.rb
@@ -1,7 +1,10 @@
 class Selection
 
-  cattr_reader :DEFAULT_USER
+  cattr_reader :COMPLETE_FIELDS_USER
   @@COMPLETE_FIELDS_USER="email, username, birth_date, location, bio, sign_up_at, full_avatar_url, large_avatar_url, medium_avatar_url, thumb_avatar_url"
+
+  cattr_reader :PREVIEW_FIELDS_USER
+  @@PREVIEW_FIELDS_USER="email, username, location, full_avatar_url, large_avatar_url, medium_avatar_url, thumb_avatar_url"
 
   cattr_reader :PROFILE
   @@PROFILE="id, "+ @@COMPLETE_FIELDS_USER
@@ -11,7 +14,7 @@ class Selection
 
 
   cattr_reader :FRIEND
-  @@FRIEND="friend_id, email, username, location, full_avatar_url, large_avatar_url, medium_avatar_url, thumb_avatar_url"
+  @@FRIEND="friend_id"+@@PREVIEW_FIELDS_USER
 
 
   cattr_reader :FRIEND_BY_ID
@@ -19,7 +22,7 @@ class Selection
 
 
   cattr_reader :FRIEND_REQUEST
-  @@FRIEND_REQUEST="friend_request_id, email, username"
+  @@FRIEND_REQUEST="friend_request_id"+@@PREVIEW_FIELDS_USER
 
 
   cattr_reader :FRIEND_REQUEST_BY_ID
