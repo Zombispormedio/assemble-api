865fca9a1e020dd7b07e6b53744b81c17563c0df
Xavier Serrano <zombispormedio007@gmail.com>
2016-10-04 11:02:31 +0200

    message serializer

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,11 +1,23 @@
 class MessageSerializer < ActiveModel::Serializer
-    attributes :id, :sender, :recipient, :created_at, :content, :is_read, :is_sent, :is_delivered
+  attributes :id, :sender, :recipient, :created_at, :content, :is_read, :is_sent, :is_delivered
 
-    def sender
-      SenderSerializer.new(object.sender).attributes
-    end
+  def sender
+    SenderSerializer.new(object.sender).attributes
+  end
 
-    def recipient
-      SenderSerializer.new(object.recipient).attributes
-    end
+  def recipient
+    SenderSerializer.new(object.recipient).attributes
+  end
+
+  def is_read
+    object.is_read==nil ? false : object.is_read
+  end
+
+  def is_sent
+    object.is_sent==nil ? false : object.is_sent
+  end
+
+  def is_delivered
+    object.is_delivered==nil ? false : object.is_delivered
+  end
 end
