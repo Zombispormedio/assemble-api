0403358e2980d606785edf4440dd1dd6527f994c
Xavier Serrano <zombispormedio007@gmail.com>
2016-10-10 10:14:41 +0200

    no last message

--- app/serializers/chat_serializer.rb
+++ app/serializers/chat_serializer.rb
@@ -1,9 +1,3 @@
 class ChatSerializer < ActiveModel::Serializer
-  attributes :id, :friend_id, :owner_id, :last_message
-  def last_message
-    if object.messages.size >0
-      last=object.messages.order("created_at ASC").last
-      MessageSerializer.new(last).attributes
-    end
-  end
+  attributes :id, :friend_id, :owner_id
 end
