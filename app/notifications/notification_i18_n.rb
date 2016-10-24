deb5a6c60ff7bcea88bd47a181b11f1da49e3391
Xavier Serrano <zombispormedio007@gmail.com>
2016-10-24 17:46:38 +0200

    es

--- app/notifications/notification_i18_n.rb
+++ app/notifications/notification_i18_n.rb
@@ -4,8 +4,8 @@ module NotificationI18N
   MESSAGE_TEMPLATE={
       android_group: "message",
       android_group_message: {
-          en: "$[notif_count] new messages",
-          es: "$[notif_count] mensajes nuevos"
+          es: "$[notif_count] new messages",
+          en: "$[notif_count] mensajes nuevos"
       },
       small_icon: "logo",
       android_accent_color: "2196f3"
